7ef23d87b7ebdc94e3d13032ee37302d72316a0d
eugen <eugen.haidukov@gmail.com>
2019-03-18 19:24:35 +0300

    fix bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,55 @@
 $(document).ready(function () {
     console.log('run')
 })
+
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+//   });
+
+$(function () {
+  $('.carousel').carousel({ interval:6000 });  
+
+  $('#playButton').click(function () {
+      $('#carouselExampleIndicators').carousel('cycle');
+  });
+  $('#pauseButton').click(function () {
+      $('#carouselExampleIndicators').carousel('pause');
+  });
+  $("button").click(function() {
+  if ($(this).attr("id") === "pauseButton") {
+      $('#homeCarousel').carousel('pause');
+      $(this).attr("id", "playButton");
+      $("span", this).toggleClass("glyphicon-play glyphicon-pause");
+  } else {
+      $('#homeCarousel').carousel('cycle');
+      $(this).attr("id", "pauseButton");
+      $("span", this).toggleClass("glyphicon-pause glyphicon-play");
+  }
+  });
+
+  "use strict";
+  $('.nav__title').click(function(){
+      $('.responsive__navbar')
+        .find('.nav__title.open, .nav__link.open')
+        .not($(this))
+        .not($(this).next('.nav__link'))
+        .removeClass('open');
+      $(this).toggleClass('open');
+      $(this).next('.nav__link').toggleClass('open');
+  });
+
+  $('.nav__menu_btn').click(function(){
+      $('#background').toggleClass('open');
+      $('#responsive').toggleClass('open');
+  });
+
+  $('#background').click(function(){
+      $('#background').toggleClass('open');
+      $('#responsive').toggleClass('open');
+  });
+  
+
+
+  
+
+});
